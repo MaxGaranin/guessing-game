2b23374b4b9df71bdf1b69fc047d70edaeace104
GaraninMS <GaraninMS@samnipineft.ru>
2019-10-09 11:31:07 +0400

    feat: add realization of task 'guessing-game'

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.current = this._getMean();
     }
 
     guess() {
-
+        return this.current;
     }
 
     lower() {
-
+        this.max = this.current;
+        this.current = this._getMean();
     }
 
     greater() {
+        this.min = this.current;
+        this.current = this._getMean();
+    }
 
+    _getMean() {
+        return Math.round((this.min + this.max) / 2);
     }
 }
 
